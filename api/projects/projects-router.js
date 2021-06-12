d8346414c3db7e946da2f2ef1506c81ba0e1bdc7
Lakendria <kendra939@gmail.com>
2021-06-12 18:20:25 -0400

    setting up server

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const router = express.Router()
+//MIDDLEWARE
+
+
+//ROUTER ENDPOINTS
+
+
+
+
+modules.exports=router
+
+
+
--- api/server.js
+++ api/server.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const server = express();
 
+//GLOBAL MIDDLEWARE
+server.use(express.json())
 // Configure your server here
+server.use("*",(req,res)=>{
+    res.status(200).json({message:"Hi"})
+    })
+
+    //ENDPOINTS
+    server.get('/api/projects', (req,res)=>{
+    
+    })
+    server.get('/api/projects/:id', (req,res)=>{
+        
+    })
+    server.post('/api/projects', (req,res)=>{
+        
+    })
+    server.put('/api/projects/:id', (req,res)=>{
+        
+    })
+    
+    server.delete('/api/projects/:id', (req,res)=>{
+        
+    })
+    server.get('/api/projects/:id/actions', (req,res)=>{
+        
+    })
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
